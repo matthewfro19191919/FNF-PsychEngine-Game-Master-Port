a60134139c2bbdbafd5b0de8150058b263b5a018
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-01-11 06:29:35 -0500

    Update TheGamingHall.hx

--- source/states/stages/TheGamingHall.hx
+++ source/states/stages/TheGamingHall.hx
@@ -64,23 +64,24 @@ class TheGamingHall extends BaseStage
 			frontGaming.setGraphicSize(Std.int(frontGaming.width * 1.5));
 			add(frontGaming);
 
-	if (SONG.song.toLowerCase() == 'jovial')
-	{
+                if (SONG.song.toLowerCase() == 'jovial')
+                {
 			frontGaming.color = FlxColor.PURPLE;
 			behindGaming.color = 0xFF823C25;
 			boyfriendGroup.color = FlxColor.PURPLE;
 			gfGroup.color = FlxColor.PURPLE;
 			dadGroup.color = FlxColor.PURPLE;
-	}
-  if (SONG.song.toLowerCase() == 'merriment' || SONG.song.toLowerCase() == 'games' || SONG.song.toLowerCase() == 'jovial')
-	{
+        	}
+                if (SONG.song.toLowerCase() == 'merriment' || SONG.song.toLowerCase() == 'games' || SONG.song.toLowerCase() == 'jovial')
+                {
 			entityBFMonster();
 			vignette.setGraphicSize(Std.int(hearts.width * 2.15));
 			vignette.color = FlxColor.PINK;
 			if (bfEntity.x <= jumpDis)
 			{
 				JSActive = true;
-		  }
+		        }
+                }
 	}
 
 	// some custom below
@@ -471,143 +472,5 @@ class TheGamingHall extends BaseStage
 			randomizeSymbols.y += 120;
 			badLuckSymbols.y = 580;
 		}
-	}
-
-	override function createPost()
-	{
-		// Use this function to layer things above characters!
-	}
-
-	override function update(elapsed:Float)
-	{
-		// Code here
-	}
-
-	override function destroy()
-	{
-		// Code here
-	}
-
-	
-	override function countdownTick(count:Countdown, num:Int)
-	{
-		switch(count)
-		{
-			case THREE: //num 0
-			case TWO: //num 1
-			case ONE: //num 2
-			case GO: //num 3
-			case START: //num 4
-		}
-	}
-
-	override function startSong()
-	{
-		// Code here
-	}
-
-	// Steps, Beats and Sections:
-	//    curStep, curDecStep
-	//    curBeat, curDecBeat
-	//    curSection
-	override function stepHit()
-	{
-		// Code here
-	}
-	override function beatHit()
-	{
-		// Code here
-	}
-	override function sectionHit()
-	{
-		// Code here
-	}
-
-	// Substates for pausing/resuming tweens and timers
-	override function closeSubState()
-	{
-		if(paused)
-		{
-			//timer.active = true;
-			//tween.active = true;
-		}
-	}
-
-	override function openSubState(SubState:flixel.FlxSubState)
-	{
-		if(paused)
-		{
-			//timer.active = false;
-			//tween.active = false;
-		}
-	}
-
-	// For events
-	override function eventCalled(eventName:String, value1:String, value2:String, flValue1:Null<Float>, flValue2:Null<Float>, strumTime:Float)
-	{
-		switch(eventName)
-		{
-			case "My Event":
-		}
-	}
-	override function eventPushed(event:objects.Note.EventNote)
-	{
-		// used for preloading assets used on events that doesn't need different assets based on its values
-		switch(event.event)
-		{
-			case "My Event":
-				//precacheImage('myImage') //preloads images/myImage.png
-				//precacheSound('mySound') //preloads sounds/mySound.ogg
-				//precacheMusic('myMusic') //preloads music/myMusic.ogg
-		}
-	}
-	override function eventPushedUnique(event:objects.Note.EventNote)
-	{
-		// used for preloading assets used on events where its values affect what assets should be preloaded
-		switch(event.event)
-		{
-			case "My Event":
-				switch(event.value1)
-				{
-					// If value 1 is "blah blah", it will preload these assets:
-					case 'blah blah':
-						//precacheImage('myImageOne') //preloads images/myImageOne.png
-						//precacheSound('mySoundOne') //preloads sounds/mySoundOne.ogg
-						//precacheMusic('myMusicOne') //preloads music/myMusicOne.ogg
-
-					// If value 1 is "coolswag", it will preload these assets:
-					case 'coolswag':
-						//precacheImage('myImageTwo') //preloads images/myImageTwo.png
-						//precacheSound('mySoundTwo') //preloads sounds/mySoundTwo.ogg
-						//precacheMusic('myMusicTwo') //preloads music/myMusicTwo.ogg
-					
-					// If value 1 is not "blah blah" or "coolswag", it will preload these assets:
-					default:
-						//precacheImage('myImageThree') //preloads images/myImageThree.png
-						//precacheSound('mySoundThree') //preloads sounds/mySoundThree.ogg
-						//precacheMusic('myMusicThree') //preloads music/myMusicThree.ogg
-				}
-		}
-	}
-
-	// Note Hit/Miss
-	override function goodNoteHit(note:Note)
-	{
-		// Code here
-	}
-
-	override function opponentNoteHit(note:Note)
-	{
-		// Code here
-	}
-
-	override function noteMiss(note:Note)
-	{
-		// Code here
-	}
-
-	override function noteMissPress(direction:Int)
-	{
-		// Code here
 	}
 }
